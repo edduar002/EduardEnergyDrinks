25e2706457cebd5baf834b63354237acb69ddbe0
edduar002 <128913974+edduar002@users.noreply.github.com>
2024-09-29 16:56:07 -0500

    Se corrije error de boton de eliminar productos tanto en administrador como en usuario, ademas de agregar documentacion de controlador de network.

--- controllers/NetworkController.php
+++ controllers/NetworkController.php
@@ -1,7 +1,7 @@
 <?php
 
     /*
-    Clase controlador de pago
+    Clase controlador de red
     */
 
     /*Incluir el modelo*/
@@ -9,43 +9,44 @@
 
     class NetworkController{
 
-        /*Funcion para abrir ventana de registro*/
+        /*Funcion para abrir ventana de agregar usuario a la red*/
         public function windowAddUser(){
             /*Incluir la vista*/
             require_once "views/network/AddUser.html";
         }
 
-        /*Funcion para abrir ventana de registro*/
+        /*Funcion para agregar usuario a la red*/
         public function addUser(){
-            /*Comprobar si llegan los datos del formulario enviados por post*/
+            /*Comprobar si llega el dato del formulario enviado por post*/
             if (isset($_POST)) {
-                /*Asignar los datos si llegan*/
+                /*Obtener id del usuario logueado*/
                 $userId = $_SESSION['loginsucces']['USER_ID'];
+                /*Asignar el dato si llega*/
                 $code = isset($_POST['code']) ? $_POST['code'] : false;
-                /*Comprobar si los datos llegan*/
+                /*Comprobar si el dato llega*/
                 if ($code) {
                     /*Instanciar modelo*/      
                     $model = new Model();
-                    /*Llamar la funcion del modelo que registra el pago*/  
+                    /*Llamar la funcion del modelo que agrega el usuario a la red*/  
                     $resultado = $model->addUser($userId, $code);
                     /*Comprobar si el registrado ha sido exitoso*/                  
                     if ($resultado != false) {
                         /*Crear la sesion y redirigir a la ruta pertinente*/
-                        Helps::createSessionAndRedirect("registrosucces", "Se ha registrado exitosamente el pago", "?controller=userController&action=managementPays");
+                        Helps::createSessionAndRedirect("aciertoasignacion", "El usuario ha sido asignado a tu red exitosamente", "?controller=networkController&action=windowAddUser");
                     /*De lo contrario*/  
                     } else {
                         /*Crear la sesion y redirigir a la ruta pertinente*/
-                        Helps::createSessionAndRedirect("registroerror", "Ha ocurrido un error al realizar el registro de la direccion", "?controller=payController&action=windowRegister");
+                        Helps::createSessionAndRedirect("errorasignacion", "Ha ocurrido un error al realizar la asignacion del usuario a tu red", "?controller=networkController&action=windowAddUser");
                     }
                 /*De lo contrario*/  
                 } else {
                     /*Crear la sesion y redirigir a la ruta pertinente*/
-                    Helps::createSessionAndRedirect("registroerror", "Ha ocurrido un error al realizar el registro de la direccion", "?controller=payController&action=windowRegister");
+                    Helps::createSessionAndRedirect("errorasignacion", "Ha ocurrido un error inesperado", "?controller=networkController&action=windowAddUser");
                 }
             /*De lo contrario*/  
             } else {
                 /*Crear la sesion y redirigir a la ruta pertinente*/
-                Helps::createSessionAndRedirect("registroerror", "Ha ocurrido un error inesperado", "?controller=payController&action=windowRegister");
+                Helps::createSessionAndRedirect("errorasignacion", "Ha ocurrido un error inesperado", "?controller=networkController&action=windowAddUser");
             }
         }
 
